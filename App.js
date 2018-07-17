92ccf76716e8600643a74476a52f1d866923503f
Karan Uppal <karan.uppal@myntra.com>
2018-07-17 12:51:50 +0530

    added input and button

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+
+constructor(props) {
+    super(props);
+    this.state = { text: 'Search Something' };
+  }
+
+  onPressLearnMore() {
+  }
+
   render() {
+
     return (
       <View style={styles.container}>
         <Text>HELLO WORLD!</Text>
+        <TextInput
+                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+        />
+        <Button
+          onPress={this.onPressLearnMore}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
       </View>
     );
   }
